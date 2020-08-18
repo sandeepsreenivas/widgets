0e50846fc00613f213d33c80812c42a979a60aa0
sandeep sreenivas <sandeepsreenivas@sandeeps-MacBook-Air.local>
2020-08-18 13:16:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Color Blue',
+        value: 'blue'
+    }
+
+]
+
+const items = [
+    {
+        title: 'What is React ?',
+        content: 'React is JavaScript Front End Framework'
+    },
+    {
+        title: 'Why use React ?',
+        content: 'For Frontend Development'
+    },
+    {
+        title: 'What is JavaScript',
+        content: 'Client-Side Programming Language'
+    }
+]
+
+export default () => {
+
+    const [selected, setSelected] = useState(options[0]);
+
+    return (
+        <div>
+            <Search />
+        </div>
+    );
 }
-
-export default App;
